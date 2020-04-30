c90e548451e427bdbe1bbd8e85a2b677d84d15b6
Shubham Lohiya <shubhlohiya.14@gmail.com>
2020-04-30 23:07:18 +0530

    Added home page

---
+++ src/learning_logs/urls.py
@@ -0,0 +1,10 @@
+"""Define URL patterns for learning logs"""
+
+from django.urls import path
+from . import views
+
+app_name = 'learning_logs'
+
+urlpatterns = [
+	path('', views.index, name='index') #Home page
+]
