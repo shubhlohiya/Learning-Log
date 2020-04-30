787194970187f71740e86d0d43e1f577a2b74142
Shubham Lohiya <shubhlohiya.14@gmail.com>
2020-05-01 00:07:56 +0530

    Added base template and topics template

--- src/learning_logs/urls.py
+++ src/learning_logs/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name = 'learning_logs'
 
 urlpatterns = [
-	path('', views.index, name='index') #Home page
+	path('', views.index, name='index'), #Home page
+	path('topics/', views.topics, name='topics'),
 ]
--- src/learning_logs/views.py
+++ src/learning_logs/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 def index(request):
 	"""The Learning Log home page"""
 
 	return render(request, 'learning_logs/index.html')
+
+def topics(request):
+	"""Shows all the topics"""
+
+	topics = Topic.objects.order_by('date_added')
+	context = {'topics': topics}
+	return render(request, 'learning_logs/topics.html', context)
